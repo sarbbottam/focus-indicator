9d31c523c493a1f16698ed16ada07d6d72e2520f
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-17 12:39:08 -0700

    feat(*): updated implementation

--- src/chrome/index.js
+++ src/chrome/index.js
@@ -1,9 +1,53 @@
+function getDimension(element) {
+  return {
+    height: `${element.offsetHeight}px`,
+    width: `${element.offsetWidth}px`
+  }
+}
+
+// http://javascript.info/tutorial/coordinates
+function getPosition(element) {
+  const rect = element.getBoundingClientRect();
+  const body = document.documentElement || document.body;
+
+  const scrollTop = window.pageYOffset || body.scrollTop;
+  const scrollLeft = window.pageXOffset || body.scrollLeft;
+
+  const clientTop = body.clientTop || 0;
+  const clientLeft = body.clientLeft || 0;
+
+  const top  = rect.top +  scrollTop - clientTop;
+  const left = rect.left + scrollLeft - clientLeft;
+
+  return {
+    top: `${Math.round(top)}px`,
+    left: `${Math.round(left)}px`
+  }
+}
+
+function displayFocusIndicator(target) {
+  const dimension = getDimension(target);
+  const position = getPosition(target);
+
+  div.style.height = dimension.height;
+  div.style.width = dimension.width;
+
+  div.style.top = position.top;
+  div.style.left = position.left;
+}
+
 function addFocusIndicator(e) {
-  e.target.classList.add('focus-indicator');
+  const target = e.target;
+  displayFocusIndicator(target);
+  // e.target.classList.add('focus-indicator');
 }
 document.addEventListener('focus', addFocusIndicator, true);
 
-function removeFocusIndicator(e) {
-  e.target.classList.remove('focus-indicator');
-}
-document.addEventListener('blur', removeFocusIndicator, true);
+// function removeFocusIndicator(e) {
+//   e.target.classList.remove('focus-indicator');
+// }
+// document.addEventListener('blur', removeFocusIndicator, true);
+
+const div = document.createElement('div');
+div.classList.add('focus-indicator');
+document.body.appendChild(div);
