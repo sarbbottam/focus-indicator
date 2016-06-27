a07b81b1658b3b04e82bb3a31e4af32815127794
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-27 10:31:27 -0700

    feat(style): generate style (#18)
    to customize focus indicator color with options.color

---
+++ src/js/get-style-node.js
@@ -0,0 +1,35 @@
+function getStyleContent(options) {
+  return `
+    @keyframes outline-pulse {
+      from {
+        outline-color:  ${options.color};
+        outline-offset: -2px;
+      }
+
+      90% {
+        outline-color: transparent;
+        outline-offset: -1px;
+      }
+
+      to {
+        outline-color: ${options.color};
+        outline-offset: 0;
+      }
+    }
+
+    :focus {
+      outline: 2px solid ${options.color} !important;
+      outline-offset: 0 !important;
+      animation: outline-pulse 4s;
+    }
+  `;
+}
+
+function getStyleNode(options) {
+  const styleNode = document.createElement('style');
+  styleNode.setAttribute('data-ref', 'focus-indicator');
+  styleNode.innerHTML = getStyleContent(options);
+  return styleNode;
+}
+
+module.exports = getStyleNode;
