8c51e39bd4d608586f891a31cb5384a55b58c142
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-28 15:28:28 -0700

    feat(options): better UI for preference page

--- src/js/options.js
+++ src/js/options.js
@@ -4,9 +4,17 @@ const optionsForm = document.getElementById('options-form');
 
 function getOptionsHTML(options) {
   return `
-    <input id="color" type="color"  value="${options.color}" />
-    <input id="width" type="number"  value="${options.width.replace('px', '')}" min="2" max="8"/>
-    <button type="submit">Save</button>
+    <div class="row">
+      <label for="color">Choose your preferred color?</label>
+      <input id="color" type="color"  value="${options.color}" />
+    </div>
+    <div class="row">
+      <label for="width">Provide your preferred width?</label>
+      <input id="width" type="number"  value="${options.width.replace('px', '')}" min="2" max="8"/>
+    </div>
+    <div>
+      <button class="button" type="submit">Save</button>
+    </div>
   `;
 }
 
