10b6a543b51022d92e9df95af1d356810f0a92f7
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-28 17:13:56 -0700

    feat(reset): to default options

--- src/js/options.js
+++ src/js/options.js
@@ -12,21 +12,38 @@ function getOptionsHTML(options) {
       <label for="width">Provide your preferred width?</label>
       <input id="width" type="number"  value="${options.width.replace('px', '')}" min="2" max="8"/>
     </div>
-    <div>
-      <button class="button" type="submit">Save</button>
+    <div class="row-multiple">
+      <div class="col-1-2 p-e-4px">
+        <button class="button primary" type="submit" id="update">Update</button>
+      </div>
+      <div class="col-1-2 p-s-4px">
+        <button class="button secondary" type="submit" id="reset">Reset</button>
+      </div>
     </div>
   `;
 }
 
 function injectOptionsHTML(options) {
   optionsForm.innerHTML = getOptionsHTML(options);
+  const resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click', resetOptions);
 }
 
-function saveOptions() {
+function saveOptions(e) {
+  e.preventDefault();
+  e.stopPropagation();
   chrome.storage.local.set({
     color: document.getElementById('color').value,
     width: `${document.getElementById('width').value}px`
   }, () => {});
+}
+
+function resetOptions(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  document.getElementById('color').value = FOCUS_INDICATOR.COLOR;
+  document.getElementById('width').value = FOCUS_INDICATOR.WIDTH.replace('px', '');
+  saveOptions(e);
 }
 
 function fetchOptions() {
@@ -38,3 +55,4 @@ function fetchOptions() {
 
 document.addEventListener('DOMContentLoaded', fetchOptions);
 optionsForm.addEventListener('submit', saveOptions);
+
