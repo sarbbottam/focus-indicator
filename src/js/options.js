4184758e0436d784f3a24cd6660566a24d7fb32d
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-29 19:09:38 -0700

    refactor(options): layout and heading

--- src/js/options.js
+++ src/js/options.js
@@ -2,19 +2,21 @@ const FOCUS_INDICATOR = require('./constant.js');
 
 function getOptionsHTML(options) {
   return `
-    <div class="row">
-      <label for="color">Choose your preferred color?</label>
-      <input id="color" type="color"  value="${options.color}" />
-    </div>
-    <div class="row">
-      <label for="width">Provide your preferred width?</label>
-      <input id="width" type="number"  value="${options.width.replace('px', '')}" min="2" max="8"/>
+    <div class="row-multiple">
+      <div class="col col-1-2 p-e-4px">
+        <label for="color">Color</label>
+        <input id="color" type="color"  value="${options.color}" />
+      </div>
+      <div class="col col-1-2 p-e-4px">
+        <label for="width">Width</label>
+        <input id="width" type="number"  value="${options.width.replace('px', '')}" min="2" max="8"/>
+      </div>
     </div>
     <div class="row-multiple">
-      <div class="col-1-2 p-e-4px">
+      <div class="col col-1-2 p-e-4px">
         <button class="button primary" type="submit" id="update">Update</button>
       </div>
-      <div class="col-1-2 p-s-4px">
+      <div class="col col-1-2 p-s-4px">
         <button class="button secondary" type="submit" id="reset">Reset</button>
       </div>
     </div>
